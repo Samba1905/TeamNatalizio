92d4811d062eb010aa9f7eac14435289544936c3
Samba1905 <131000872+Samba1905@users.noreply.github.com>
2023-11-21 16:37:40 +0100

    MainMenu in corso
    Aggiunti tutti i passaggi per il MainMenu e qualcosa per Audio

--- Team Natalizio/Assets/Script/Manager/MainMenuManager.cs
+++ Team Natalizio/Assets/Script/Manager/MainMenuManager.cs
@@ -1,44 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    #region Singleton
-    private static MainMenuManager _instance;
-    public static MainMenuManager Instance
-    {
-        get
-        {
-            if (_instance == null) Debug.Log("MainMenuManager null");
-            return _instance;
-        }
-    }
-    #endregion
+    [Header("MainMenuButtons")]
+    public GameObject continueButton;
+    public GameObject newGameButton, optionsButton, creditsButton, exitButton;
+    [Header("Panels")]
+    public GameObject optionPanel;
+    public GameObject videoPanel, audioPanel, gamePanel;
 
-    private void Awake()
-    {
-        #region Singleton
-        if (_instance) Destroy(gameObject);
-        else _instance = this;
-
-        DontDestroyOnLoad(gameObject);
-        #endregion
-    }
-
+    #region MainMenu
     public void ContinueButton()
     {
-
+        //Funzione di caricamneto ultima scena/salvataggio
     }
 
     public void NewGameButton()
     {
-
+        //SceneManager.LoadScene(); Carica scena e crea salvataggio
     }
 
-    public void OptionsButton()
+    public void OptionsButton() //Apre menu opzioni
     {
-
+        newGameButton.SetActive(false);
+        continueButton.SetActive(false);
+        optionsButton.SetActive(false);
+        creditsButton.SetActive(false);
+        exitButton.SetActive(false);
+        optionPanel.SetActive(true);
     }
 
     public void CreditsButton()
@@ -46,8 +40,59 @@ public class MainMenuManager : MonoBehaviour
 
     }
 
-    public void ExitButton()
+    public void ExitButton() //Chiude il gioco
     {
         Application.Quit();
     }
+    #endregion
+    #region Options
+    public void OptionsVideo()
+    {
+        optionPanel.SetActive(false);
+        videoPanel.SetActive(true);
+    }
+
+    public void OptionsAudio()
+    {
+        optionPanel.SetActive(false);
+        audioPanel.SetActive(true);
+    }
+
+    public void OptionsGame()
+    {
+        optionPanel.SetActive(false);
+        gamePanel.SetActive(true);
+    }
+
+    public void BackToMainMenuFromOptions()
+    {
+        newGameButton.SetActive(true);
+        continueButton.SetActive(true);
+        optionsButton.SetActive(true);
+        creditsButton.SetActive(true);
+        exitButton.SetActive(true);
+        optionPanel.SetActive(false);
+    }
+    #endregion
+    #region Video
+    public void BackToOptionsFromVideo()
+    {
+        videoPanel.SetActive(false);
+        optionPanel.SetActive(true);
+    }
+    #endregion
+    #region Audio
+    public void BackToOptionsFromAudio()
+    {
+        audioPanel.SetActive(false);
+        optionPanel.SetActive(true);
+    }
+    #endregion
+    #region Game
+    public void BackToOptionsFromGame()
+    {
+        gamePanel.SetActive(false);
+        optionPanel.SetActive(true);
+    }
+    #endregion
 }
