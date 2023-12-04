802dfde91740b22fc689d99839b0ba7112866796
Samba1905 <131000872+Samba1905@users.noreply.github.com>
2023-12-04 16:53:11 +0100

    Renna
    Iniziato ad inserire le animazioni renna e la modalità attacco, il collider disturba l'animazione e blocca il personaaggio, la modalità attacco è da sistemare

--- Team Natalizio/Assets/Script/Enemy/SimpleEnemy/Deer.cs
+++ Team Natalizio/Assets/Script/Enemy/SimpleEnemy/Deer.cs
@@ -7,6 +7,7 @@ public class Deer : MonoBehaviour
 {
     Transform player;
     Rigidbody rb;
+    Animator anim;
     [SerializeField, Header("Speed")]
     float moveSpeed;
     [SerializeField]
@@ -15,6 +16,9 @@ public class Deer : MonoBehaviour
     float rayDist;
     [SerializeField]
     bool isWalking, isRunning, isAttacking;
+    [SerializeField]
+    bool takePosPlayer;
+    Vector3 playerPosAttack;
 
     [SerializeField]
     Transform [] waypoints;
@@ -22,22 +26,35 @@ public class Deer : MonoBehaviour
     [SerializeField]
     float distance, idleTimer;
 
+    enum DeerState
+    {
+        normal,
+        attack
+    }
 
-
-
+    DeerState state;
 
     private void Start()
     {
         player = FindAnyObjectByType<Player>().GetComponentInParent<Transform>();
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        state = DeerState.normal;
     }
 
     private void Update()
     {
-        Movement();
+        if (state == DeerState.normal)
+        {
+            Movement();
+        }
+        else if(state == DeerState.attack)
+        {
+            AttackMode();
+        }
+        animUpdate();
+        DetectPlayer();
     }
-
-
 
     void Movement()
     {
@@ -58,6 +75,7 @@ public class Deer : MonoBehaviour
         if(distance < 0.25f && distance > -0.25f)
         {
             moveSpeed = 0f;
+            isWalking = false;
             idleTimer += Time.deltaTime;
             if(idleTimer > 4.5f)
             {
@@ -68,17 +86,50 @@ public class Deer : MonoBehaviour
         else
         {
             moveSpeed = walkSpeed;
+            isWalking = true;
         }
+    }
+
+    void animUpdate()
+    {
+        anim.SetBool("isWalking", isWalking);
+        //anim.SetBool("isRunning", isRunning); DA SISTEMARE
     }
 
     void DetectPlayer()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = new Ray(transform.position + Vector3.up, transform.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, rayDist))
         {
-            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
+            Debug.DrawRay(transform.position + Vector3.up, transform.forward * hit.distance, Color.red);
+            if (hit.collider.CompareTag("Player"))
+            {
+                state = DeerState.attack;
+                Debug.Log("Player");
+            }
+        }
+    }
+
+    void AttackMode()
+    {
+        isWalking = false;
+        isRunning = true;
+        moveSpeed = runSpeed;
+
+        if(!takePosPlayer)
+        {
+            playerPosAttack = player.transform.position + new Vector3(-7f, 0f, 0f);
+            isAttacking = true;
+            takePosPlayer = true;
+        }
+
+        if(isAttacking)
+        {
+            float distance = playerPosAttack.x - transform.position.x;
+            Debug.Log(distance);
+            if(distance < 0.5f && distance > -0.5f) rb.velocity = new Vector3(-moveSpeed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
         }
     }
 }
