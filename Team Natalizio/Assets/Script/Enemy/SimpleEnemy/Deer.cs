5095aa56f90b5dcbb345c13a169ea99edb97fabc
Samba1905 <131000872+Samba1905@users.noreply.github.com>
2023-12-06 18:08:32 +0100

    Renna attacca Player
    Corretto e sistemato movimento e attacco Renna verso player, manca solo da aggiungere lo script di danno (semplice)

--- Team Natalizio/Assets/Script/Enemy/SimpleEnemy/Deer.cs
+++ Team Natalizio/Assets/Script/Enemy/SimpleEnemy/Deer.cs
@@ -19,12 +19,14 @@ public class Deer : MonoBehaviour
     [SerializeField]
     bool takePosPlayer;
     Vector3 playerPosAttack;
+    [SerializeField]
+    bool directionR, restTime;
 
     [SerializeField]
     Transform [] waypoints;
     int nWay;
     [SerializeField]
-    float distance, idleTimer;
+    float distance, idleTimer, distancePlayer, restTimer;
 
     enum DeerState
     {
@@ -32,6 +34,7 @@ public class Deer : MonoBehaviour
         attack
     }
 
+    [SerializeField]
     DeerState state;
 
     private void Start()
@@ -46,38 +49,44 @@ public class Deer : MonoBehaviour
     {
         if (state == DeerState.normal)
         {
-            Movement();
+            Rotation();
         }
-        else if(state == DeerState.attack)
+        else if (state == DeerState.attack)
         {
-            AttackMode();
+            ChargeMode();
+            RestTimer();
+            ChangeState();
         }
         animUpdate();
         DetectPlayer();
+        DetectRotation();
     }
 
-    void Movement()
+    private void FixedUpdate()
     {
-        if (transform.localEulerAngles.y > 180f)
+        if (state == DeerState.normal)
         {
-            rb.velocity = new Vector3(-moveSpeed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+            Movement();
         }
-        else
+        else if (state == DeerState.attack)
         {
-            rb.velocity = new Vector3(moveSpeed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+            AttackMode();
         }
+    }
 
+    void Rotation()
+    {
         distance = waypoints[nWay].transform.position.x - transform.position.x;
 
         if (distance < 0) transform.localEulerAngles = new Vector3(0f, 270f, 0f);
         else if (distance > 0) transform.localEulerAngles = new Vector3(0f, 90f, 0f);
 
-        if(distance < 0.25f && distance > -0.25f)
+        if (distance < 0.25f && distance > -0.25f)
         {
             moveSpeed = 0f;
             isWalking = false;
             idleTimer += Time.deltaTime;
-            if(idleTimer > 4.5f)
+            if (idleTimer > 4.5f)
             {
                 nWay = (nWay + 1) % waypoints.Length;
                 idleTimer = 0f;
@@ -88,6 +97,18 @@ public class Deer : MonoBehaviour
             moveSpeed = walkSpeed;
             isWalking = true;
         }
+    }
+
+    void Movement()
+    {
+        if (directionR)
+        {
+            rb.velocity = new Vector3(moveSpeed * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+        }
+        else if(!directionR)
+        {
+            rb.velocity = new Vector3(-moveSpeed * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+        }    
     }
 
     void animUpdate()
@@ -107,7 +128,6 @@ public class Deer : MonoBehaviour
             if (hit.collider.CompareTag("Player"))
             {
                 state = DeerState.attack;
-                Debug.Log("Player");
             }
         }
     }
@@ -118,18 +138,63 @@ public class Deer : MonoBehaviour
         isRunning = true;
         moveSpeed = runSpeed;
 
-        if(!takePosPlayer)
+        if(!takePosPlayer && !directionR)
         {
-            playerPosAttack = player.transform.position + new Vector3(-7f, 0f, 0f);
+            playerPosAttack = player.transform.position + new Vector3(-5f, 0f, 0f);
             isAttacking = true;
             takePosPlayer = true;
         }
-
-        if(isAttacking)
+        else if (!takePosPlayer && directionR)
         {
-            float distance = playerPosAttack.x - transform.position.x;
-            Debug.Log(distance);
-            if(distance < 0.5f && distance > -0.5f) rb.velocity = new Vector3(-moveSpeed * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+            playerPosAttack = player.transform.position + new Vector3(5f, 0f, 0f);
+            isAttacking = true;
+            takePosPlayer = true;
+            
         }
+
+        if (isAttacking)
+        {        
+            if (distancePlayer >= 5f)
+            {
+                rb.velocity = new Vector3(moveSpeed * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+            }
+            else if(distancePlayer <= -5f)
+            {
+                rb.velocity = new Vector3(-moveSpeed * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+            }
+            else
+            {
+                restTime = true;
+            }
+        }
+    }
+
+    void ChangeState()
+    {
+        if (restTimer > 3.5f)
+        {
+            state = DeerState.normal;
+            takePosPlayer = false;
+            isRunning = false;
+            isWalking = true;
+            restTime = false;
+        }
+    }
+
+    float ChargeMode()
+    {
+        return distancePlayer = playerPosAttack.x - transform.position.x;
+    }
+
+    float RestTimer()
+    {
+        if(restTime) return restTimer += Time.deltaTime;
+        return restTimer = 0f;
+    }
+
+    bool DetectRotation()
+    {
+        if (transform.localEulerAngles.y > 180f) return directionR = false;           
+        return directionR = true;
     }
 }
